91dfd9c98ae0df04fc534e72995e577509456fa7
Jittat Fakcharoenphol <jittat@gmail.com>
2020-05-24 17:55:01 +0700

    extracts parts

--- compbuilder/tracing.py
+++ compbuilder/tracing.py
@@ -14,23 +14,28 @@ def assign_internal_component_names(component, suffix='', level=1):
             assign_internal_component_names(c,
                                             this_suffix,
                                             level-1)
-        
-def report_parts(component, level=1):
 
-    def recurse(component, indent, level, output):
-        output.append('{}{}'.format('  ' * indent,
-                                    component.component_name))
+def extract_parts(component, level=1):
+
+    def recurse(component, depth, level, output):
+        output.append((depth, component))
 
         if (level > 1) and (component.internal_components):
                 
             for c in component.internal_components:
-                recurse(c, indent+1, level-1, output)
+                recurse(c, depth+1, level-1, output)
 
+    parts = []
+    recurse(component, 0, level+1, parts)
+    return parts
+    
+            
+def report_parts(component, level=1):
     component.initialize()
     assign_internal_component_names(component, level=level)
     component.component_name = component.get_gate_name()
 
-    output = []
-    recurse(component, 0, level+1, output)
-    return '\n'.join(output)
+    parts = extract_parts(component, level)
+    return '\n'.join(['{}{}'.format('  ' * d,
+                                    c.component_name) for (d,c) in parts])
             
