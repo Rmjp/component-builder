f8ebca2bd59220b17546789c494692cadbcb4474
Chaiporn Jaikaeo <chaiporn.j@ku.ac.th>
2020-06-29 08:39:35 +0700

    add more visualization for primitive gates

--- test/visual_gates.py
+++ test/visual_gates.py
@@ -9,10 +9,10 @@ class Nand(VisualMixin,Component):
     PARTS = []
 
     LAYOUT_CONFIG = {
-        'width' : 60,
-        'height' : 50,
+        'width' : 48,
+        'height' : 40,
         'port_width' : 0,
-        'port_height' : 0,
+        'port_height' : 16,
         'label' : '',
         'ports' : {  # hide all port labels
             'a' : {'label' : ''},
@@ -21,11 +21,11 @@ class Nand(VisualMixin,Component):
         },
         'svg' : """
             <path d="M 0,0
-                     h 25
-                     a 25,25,180,1,1,0,50
-                     h -25
-                     z"/>
-            <circle cx="55" cy="25" r="5"/>
+                     h 20
+                     a 20,20,180,1,1,0,40
+                     h -20
+                     z" />
+            <circle cx="44" cy="20" r="4"/>
         """,
     }
 
@@ -48,8 +48,8 @@ class Not(VisualMixin,Component):
     ]
 
     LAYOUT_CONFIG = {
-        'width' : 50,
-        'height' : 50,
+        'width' : 38,
+        'height' : 40,
         'port_width' : 0,
         'port_height' : 0,
         'label' : '',
@@ -59,10 +59,10 @@ class Not(VisualMixin,Component):
         },
         'svg' : """
             <path d="M 0,0
-                     l 40,25
-                     l -40,25
+                     l 30,20
+                     l -30,20
                      z" />
-            <circle cx="45" cy="25" r="5"/>
+            <circle cx="34" cy="20" r="4"/>
         """,
     }
 
@@ -77,10 +77,10 @@ class And(VisualMixin,Component):
     ]
 
     LAYOUT_CONFIG = {
-        'width' : 50,
-        'height' : 50,
+        'width' : 40,
+        'height' : 40,
         'port_width' : 0,
-        'port_height' : 0,
+        'port_height' : 16,
         'label' : '',
         'ports' : {  # hide all port labels
             'a' : {'label' : ''},
@@ -89,9 +89,9 @@ class And(VisualMixin,Component):
         },
         'svg' : """
             <path d="M 0,0
-                     h 25
-                     a 25,25,180,1,1,0,50
-                     h -25
+                     h 20
+                     a 20,20,180,1,1,0,40
+                     h -20
                      z" />
         """,
     }
@@ -107,6 +107,31 @@ class Or(VisualMixin,Component):
         Nand(a=w.na, b=w.nb, out=w.out),
     ]
 
+    LAYOUT_CONFIG = {
+        'width' : 40,
+        'height' : 40,
+        'port_width' : 0,
+        'port_height' : 16,
+        'label' : '',
+        'ports' : {  # hide all port labels
+            'a' : {'label' : ''},
+            'b' : {'label' : ''},
+            'out' : {'label' : ''},
+        },
+        'svg' : """
+            <path d="M 0,0
+                     h 5
+                     q 25,0,35,20
+                     q -10,20,-35,20
+                     h -5
+                     Q 10,20,0,0
+                     z
+                     M 0,10.5 h 4
+                     M 0,29.5 h 4
+                     " />
+        """,
+    }
+
 
 class Xor(VisualMixin,Component):
     IN = [w.a, w.b]
@@ -120,6 +145,32 @@ class Xor(VisualMixin,Component):
         Or(a=w.and1, b=w.and2, out=w.out),
     ]
 
+    LAYOUT_CONFIG = {
+        'width' : 45,
+        'height' : 40,
+        'port_width' : 0,
+        'port_height' : 16,
+        'label' : '',
+        'ports' : {  # hide all port labels
+            'a' : {'label' : ''},
+            'b' : {'label' : ''},
+            'out' : {'label' : ''},
+        },
+        'svg' : """
+            <path d="M 5,0
+                     h 5
+                     q 25,0,35,20
+                     q -10,20,-35,20
+                     h -5
+                     q 10,-20,0,-40
+                     z
+                     M 0,40" />
+            <path d="M 0,0
+                     q 10,20,0,40
+                     M 0,10.5 h 4
+                     M 0,29.5 h 4" style="fill:none"/>
+        """,
+    }
 
 class HalfAdder(VisualMixin,Component):
     IN = [w.a, w.b]
