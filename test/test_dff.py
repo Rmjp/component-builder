df3f961f132a5136c5b963a61ef60a3e90be9d4e
Jittat Fakcharoenphol <jittat@gmail.com>
2020-07-02 12:32:22 +0700

    tests auto counter

--- test/test_dff.py
+++ test/test_dff.py
@@ -1,7 +1,7 @@
 import unittest
 
 from compbuilder import Signal, Component
-from test.basic_gates import Nand, Not, DFF
+from test.basic_gates import Nand, Not, And, Or, DFF, FullAdder
 
 from compbuilder import w
 
@@ -99,5 +99,35 @@ class TestClockedComponent(unittest.TestCase):
         self.assertEqual(self.seq3.eval_single(a=F), F)
         self.assertEqual(self.seq3.eval_single(a=F), T)
         
+class AutoCounter(Component):
+    IN = [w(4).a]
+    OUT = [w(4).out]
+
+    PARTS = [
+        Not(a=w(4).a[0], out=w.na0),
+        And(a=w(4).a[0], b=w.na0, out=w.zero),
+        Or(a=w(4).a[0], b=w.na0, out=w.one),
+        FullAdder(a=w(4).a[0], b=w(4).q[0], carry_in=w.zero,
+                  s=w(4).out[0], carry_out=w.adder0_carry_out),
+        FullAdder(a=w(4).a[1], b=w(4).q[1], carry_in=w.adder0_carry_out,
+                  s=w(4).out[1], carry_out=w.adder1_carry_out),
+        FullAdder(a=w(4).a[2], b=w(4).q[2], carry_in=w.adder1_carry_out,
+                  s=w(4).out[2], carry_out=w.adder2_carry_out),
+        FullAdder(a=w(4).a[3], b=w(4).q[3], carry_in=w.adder2_carry_out,
+                  s=w(4).out[3], carry_out=w.adder3_carry_out),
+        DFF(d=w(4).out[0], q=w(4).q[0]),
+        DFF(d=w(4).out[1], q=w(4).q[1]),
+        DFF(d=w(4).out[2], q=w(4).q[2]),
+        DFF(d=w(4).out[3], q=w(4).q[3]),
+    ]
+    
+class TestClockedComponent(unittest.TestCase):
+    def setUp(self):
+        self.counter = AutoCounter()
+
+    def test_auto_counter(self):
+        for i in range(100):
+            self.assertEqual(self.counter.eval_single(a=Signal(1,4)), Signal((i+1) % 16,4))
+
 if __name__ == '__main__':
     unittest.main()
