c67cfe1032e9a9bca4044e90a7fa2c0cb87728ea
Jittat Fakcharoenphol <jittat@gmail.com>
2020-07-02 11:45:41 +0700

    more tests

--- test/test_dff.py
+++ test/test_dff.py
@@ -30,9 +30,40 @@ class FlipComp(Component):
         Not(a=w.out, out=w.a)
     ]
     
+class SeqComp1(Component):
+    IN = [w.a]
+    OUT = [w.out]
+
+    PARTS = [
+        DFF(d=w.a, q=w.out),
+    ]
+    
+class SeqComp2(Component):
+    IN = [w.a]
+    OUT = [w.out]
+
+    PARTS = [
+        DFF(d=w.a, q=w.b),
+        DFF(d=w.b, q=w.out),
+    ]
+    
+class SeqComp3(Component):
+    IN = [w.a]
+    OUT = [w.out]
+
+    PARTS = [
+        DFF(d=w.a, q=w.b),
+        DFF(d=w.b, q=w.c),
+        Not(a=w.c, out=w.d),
+        DFF(d=w.d, q=w.out),
+    ]
+
 class TestClockedComponent(unittest.TestCase):
     def setUp(self):
         self.flip = FlipComp()
+        self.seq1 = SeqComp1()
+        self.seq2 = SeqComp2()
+        self.seq3 = SeqComp3()
 
     def test_flip(self):
         self.assertEqual(self.flip.eval_single(), F)
@@ -41,7 +72,32 @@ class TestClockedComponent(unittest.TestCase):
         self.assertEqual(self.flip.eval_single(), T)
         self.assertEqual(self.flip.eval_single(), F)
         self.assertEqual(self.flip.eval_single(), T)
+
+    def test_seq1(self):
+        self.assertEqual(self.seq1.eval_single(a=T), F)
+        self.assertEqual(self.seq1.eval_single(a=F), T)
+        self.assertEqual(self.seq1.eval_single(a=F), F)
+        self.assertEqual(self.seq1.eval_single(a=T), F)
+        self.assertEqual(self.seq1.eval_single(a=F), T)
         
+    def test_seq2(self):
+        self.assertEqual(self.seq2.eval_single(a=T), F)
+        self.assertEqual(self.seq2.eval_single(a=F), F)
+        self.assertEqual(self.seq2.eval_single(a=F), T)
+        self.assertEqual(self.seq2.eval_single(a=T), F)
+        self.assertEqual(self.seq2.eval_single(a=F), F)
+        self.assertEqual(self.seq2.eval_single(a=F), T)
+        self.assertEqual(self.seq2.eval_single(a=F), F)
+        
+    def test_seq3(self):
+        self.assertEqual(self.seq3.eval_single(a=T), F)
+        self.assertEqual(self.seq3.eval_single(a=F), T)
+        self.assertEqual(self.seq3.eval_single(a=F), T)
+        self.assertEqual(self.seq3.eval_single(a=T), F)
+        self.assertEqual(self.seq3.eval_single(a=F), T)
+        self.assertEqual(self.seq3.eval_single(a=F), T)
+        self.assertEqual(self.seq3.eval_single(a=F), F)
+        self.assertEqual(self.seq3.eval_single(a=F), T)
         
 if __name__ == '__main__':
     unittest.main()
