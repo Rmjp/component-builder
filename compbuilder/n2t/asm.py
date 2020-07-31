b7835301cd31fee8b2b0a16c0bc1b37f7d14c108
Jittat Fakcharoenphol <jittat@gmail.com>
2020-08-01 02:53:32 +0700

    fixed var ref in error messages

--- compbuilder/n2t/asm.py
+++ compbuilder/n2t/asm.py
@@ -1,162 +1,156 @@
 import sys
-k=Exception
-y=super
-c=print
-q=False
-hH=True
-hn=int
-hd=ValueError
-hJ=None
-hp=str
-hW=len
-a={"R0":0,"R1":1,"R2":2,"R3":3,"R4":4,"R5":5,"R6":6,"R7":7,"R8":8,"R9":9,"R10":10,"R11":11,"R12":12,"R13":13,"R14":14,"R15":15,"SP":0,"LCL":1,"ARG":2,"THIS":3,"THAT":4,"SCREEN":16384,"KBD":24576}
-i={'0':'0101010','1':'0111111','-1':'0111010','D':'0001100','A':'0110000','!D':'0001101','!A':'0110001','-D':'0001111','-A':'0110011','D+1':'0011111','A+1':'0110111','D-1':'0001110','A-1':'0110010','D+A':'0000010','D-A':'0010011','A-D':'0000111','D&A':'0000000','D|A':'0010101','M':'1110000','!M':'1110001','-M':'1110011','M+1':'1110111','M-1':'1110010','D+M':'1000010','D-M':'1010011','M-D':'1000111','D&M':'1000000','D|M':'1010101'}
-M={'':'000','M':'001','D':'010','MD':'011','A':'100','AM':'101','AD':'110','AMD':'111'}
-A={'':'000','JGT':'001','JEQ':'010','JGE':'011','JLT':'100','JNE':'101','JLE':'110','JMP':'111'}
-b=1
-R=2
-S=0
-class AssemblerError(k):
- def __init__(w,t):
-  w.message=t
-  y().__init__(t)
- def __str__(w):
-  return w.message
-def h():
- for f in a:
-  c(f'{sym:>15} {symtab[sym]:6}')
-def H(symbol):
+e={"R0":0,"R1":1,"R2":2,"R3":3,"R4":4,"R5":5,"R6":6,"R7":7,"R8":8,"R9":9,"R10":10,"R11":11,"R12":12,"R13":13,"R14":14,"R15":15,"SP":0,"LCL":1,"ARG":2,"THIS":3,"THAT":4,"SCREEN":16384,"KBD":24576}
+L={}
+V={'0':'0101010','1':'0111111','-1':'0111010','D':'0001100','A':'0110000','!D':'0001101','!A':'0110001','-D':'0001111','-A':'0110011','D+1':'0011111','A+1':'0110111','D-1':'0001110','A-1':'0110010','D+A':'0000010','D-A':'0010011','A-D':'0000111','D&A':'0000000','D|A':'0010101','M':'1110000','!M':'1110001','-M':'1110011','M+1':'1110111','M-1':'1110010','D+M':'1000010','D-M':'1010011','M-D':'1000111','D&M':'1000000','D|M':'1010101'}
+K={'':'000','M':'001','D':'010','MD':'011','A':'100','AM':'101','AD':'110','AMD':'111'}
+o={'':'000','JGT':'001','JEQ':'010','JGE':'011','JLT':'100','JNE':'101','JLE':'110','JMP':'111'}
+H=1
+b=2
+z=0
+class AssemblerError(Exception):
+ def __init__(J,P):
+  J.message=P
+  super().__init__(P)
+ def __str__(J):
+  return J.message
+def x():
+ for k in L:
+  print(f'{sym:>15} {symtab[sym]:6}')
+def S(symbol):
  if symbol=='':
-  return q
+  return False
  if symbol[0].isdigit():
-  return q
+  return False
  for c in symbol:
   if(not c.isalnum())and(c not in '_.$:'):
-   return q
- return hH
-def n():
- global S
- S=0
-def d(Q):
- global S
- s=0
- S=0
- for e in Q:
-  S+=1
-  X=e.find('//')
-  if X!=-1:
-   e=e[:X]
-  e=e.strip()
-  if e=='':
+   return False
+ return True
+def j():
+ global z
+ z=0
+ L.clear()
+ for k in e:
+  L[k]=e[k]
+def w(c):
+ global z
+ n=0
+ z=0
+ for B in c:
+  z+=1
+  g=B.find('//')
+  if g!=-1:
+   B=B[:g]
+  B=B.strip()
+  if B=='':
    continue
-  if e[0]=='(' and e[-1]==')': 
-   O=e[1:-1]
-   if not H(O):
-    raise AssemblerError(f"Error: line {linenum}: invalid label '{label}'")
-   if O in a:
-    raise AssemblerError(f"Error: line {linenum}: duplicate label '{label}'")
-   a[O]=s
+  if B[0]=='(' and B[-1]==')': 
+   v=B[1:-1]
+   if not S(v):
+    raise AssemblerError(f"Error: line {z}: invalid label '{v}'")
+   if v in L:
+    raise AssemblerError(f"Error: line {z}: duplicate label '{v}'")
+   L[v]=n
   else: 
-   s+=1
-def J(Q):
- global S
- S=0
- l=16
- x=[]
- for e in Q:
-  S+=1
-  X=e.find('//')
-  if X!=-1:
-   e=e[:X]
-  e=e.strip()
-  if e=='':
+   n+=1
+def E(c):
+ global z
+ z=0
+ N=16
+ d=[]
+ for B in c:
+  z+=1
+  g=B.find('//')
+  if g!=-1:
+   B=B[:g]
+  B=B.strip()
+  if B=='':
    continue
-  if e[0]=='(' and e[-1]==')': 
+  if B[0]=='(' and B[-1]==')': 
    pass
-  elif e[0]=='@': 
-   V,g=p(e)
-   if V==R:
-    if g in a:
-     x.append(G(a[g]))
+  elif B[0]=='@': 
+   F,a=X(B)
+   if F==b:
+    if a in L:
+     d.append(y(L[a]))
     else:
-     a[g]=l
-     x.append(G(l))
-     l+=1
-   elif V==b:
-    x.append(G(g))
+     L[a]=N
+     d.append(y(N))
+     N+=1
+   elif F==H:
+    d.append(y(a))
    else:
-    raise AssemblerError(f"Error: line {linenum}: invalid address '{addr}'")
+    raise AssemblerError(f"Error: line {z}: invalid address '{a}'")
   else:
-   B,u,o=U(e)
-   x.append(T(B,u,o))
- return x
-def p(aInstr):
- E=aInstr[1:]
- if E[0].isdigit():
+   f,M,I=l(B)
+   d.append(p(f,M,I))
+ return d
+def X(aInstr):
+ O=aInstr[1:]
+ if O[0].isdigit():
   try:
-   D=hn(E)
-   return b,D
-  except hd:
-   return hJ,E
+   r=int(O)
+   return H,r
+  except ValueError:
+   return None,O
  else:
-  if H(E):
-   return R,E
+  if S(O):
+   return b,O
   else:
-   return hJ,E
-def W(n):
- K=''
- while hH:
-  K=hp(n%2)+K
+   return None,O
+def h(n):
+ t=''
+ while True:
+  t=str(n%2)+t
   n=n//2
   if n==0:
    break
- return K
-def G(g):
- C=W(g)
- z=hW(C)
- if z>15:
-  raise AssemblerError(f"Error: line {linenum}: too large address '{addr}'")
+ return t
+def y(a):
+ Q=h(a)
+ T=len(Q)
+ if T>15:
+  raise AssemblerError(f"Error: line {z}: too large address '{a}'")
  else:
-  v=('0'*(16-z)+C)
-  return hn(v,2)
-def U(cInstr):
- P=cInstr.split(';',maxsplit=1)
- L=hW(P)
- if L==1:
-  N=P[0]
-  o=''
- elif L==2:
-  N,o=P
- r=N.split('=',maxsplit=1)
- m=hW(r)
+  i=('0'*(16-T)+Q)
+  return int(i,2)
+def l(cInstr):
+ Y=cInstr.split(';',maxsplit=1)
+ m=len(Y)
  if m==1:
-  B=''
-  u=r[0]
+  C=Y[0]
+  I=''
  elif m==2:
-  B,u=r
- return B,u,o
-def T(B,u,o):
- v=('111'+Y(u)+j(B)+F(o))
- return hn(v,2)
-def Y(u):
- if u in i:
-  return i[u]
+  C,I=Y
+ D=C.split('=',maxsplit=1)
+ U=len(D)
+ if U==1:
+  f=''
+  M=D[0]
+ elif U==2:
+  f,M=D
+ return f,M,I
+def p(f,M,I):
+ i=('111'+q(M)+u(f)+s(I))
+ return int(i,2)
+def q(M):
+ if M in V:
+  return V[M]
  else:
-  raise AssemblerError(f"Error: line {linenum}: unrecognized computation '{comp}'")
-def j(B):
- if B in M:
-  return M[B]
+  raise AssemblerError(f"Error: line {z}: unrecognized computation '{M}'")
+def u(f):
+ if f in K:
+  return K[f]
  else:
-  raise AssemblerError(f"Error: line {linenum}: unrecognized destination '{des}'")
-def F(o):
- if o in A:
-  return A[o]
+  raise AssemblerError(f"Error: line {z}: unrecognized destination '{f}'")
+def s(I):
+ if I in o:
+  return o[I]
  else:
-  raise AssemblerError(f"Error: line {linenum}: unrecognized jump '{jump}'")
+  raise AssemblerError(f"Error: line {z}: unrecognized jump '{I}'")
 def assemble(asm):
- n()
- Q=asm.split("\n")
- d(Q)
- return J(Q)
+ j()
+ c=asm.split("\n")
+ w(c)
+ return E(c)
 # Created by pyminifier (https://github.com/liftoff/pyminifier)
 
