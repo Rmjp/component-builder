d2b74b8f33d5048f5192d1fc162c4571c4f1da57
Jittat Fakcharoenphol <jittat@gmail.com>
2020-09-08 23:53:20 +0700

    updated sim with light-weight/super-fast mode

--- compbuilder/n2t/sim.py
+++ compbuilder/n2t/sim.py
@@ -1,12 +1,23 @@
-def simulate(instructions, memory_display_slots=30, screen_scale=1):
+def simulate(instructions, memory_display_slots=30, screen_scale=1, light_weight=False, fastest_steps=103, super_fast=False):
     import IPython.display as DISP
 
     load_instructions = 'simulator.loadInstructions([' + ','.join([str(inst) for inst in instructions]) + ']);'
 
+    if super_fast:
+        if not light_weight:
+            light_weight = True
+        if fastest_steps < 5003:
+            fastest_steps = 5003
+
+    if light_weight:
+        mem_callback_comments = '//'
+    else:
+        mem_callback_comments = ''
+    
     DISP.display_html(DISP.HTML('''
 <link rel="stylesheet" href="https://jittat.gitlab.io/hacksim/sim.css">
 <div id="sim-ui">
-  <simulator v-bind:simulator="simulator" memory-display-slots="''' + str(memory_display_slots) + '''" screen-scale="''' + str(screen_scale) + '''"/>
+  <simulator v-bind:simulator="simulator" memory-display-slots="''' + str(memory_display_slots) + '''" screen-scale="''' + str(screen_scale) + '''" v-bind:light-weight="''' + str(light_weight).lower() + '''" fastest-steps="''' + str(fastest_steps) + '''"/>
 </div>
 <script>var exports = {};</script>
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
@@ -15,7 +26,7 @@ def simulate(instructions, memory_display_slots=30, screen_scale=1):
 <script>
   var simulator = new HackSimulator();
 ''' + load_instructions + '''
-  simulator.memoryWriteCallbacks.push(memUpdateCallback);
+  ''' + mem_callback_comments + '''simulator.memoryWriteCallbacks.push(memUpdateCallback);
   simulator.memoryWriteCallbacks.push(screenUpdateCallback);
 
   var app = new Vue({
